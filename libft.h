7dd93df1624ea01e88ccf2d60adc7c8d421ae4e9
Powopaf <paf.fournier@gmail.com>
2025-11-04 22:48:07 +0100

    finihs ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pifourni <pifourni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:37:49 by pifourni          #+#    #+#             */
-/*   Updated: 2025/11/04 21:26:47 by pifourni         ###   ########.fr       */
+/*   Updated: 2025/11/04 22:34:15 by pifourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ int				ft_tolower(int c);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 int				ft_strncmp(const char *s1, const char *s2, unsigned int n);
+void			*ft_memchr(const void *s, int c, unsigned int n);
 
 #endif
